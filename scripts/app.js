df3a06039abafc8f65e033626a1a5008ce56a4fc
brouchey <benrouch@me.com>
2016-12-10 15:06:02 +0100

    add and manage item change state

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,16 @@
 angular.module("todoListApp", [])		// creates new module, second parameter defines dependencies
-.controller('mainCtrl', function($scope) {     // $scope = area of operation
-     $scope.helloWorld = function() {
-          console.log("Hello there! This is the helloWorld controller function.");
+.controller('mainCtrl', function($scope) {		// $scope = area of operation
+
+     $scope.learningNgChange = function() {		// just to see how it works
+          console.log("An input changed !");
      };
+
+     $scope.todos = [     // array of objects
+          { "name": "clean the house" },
+          { "name": "water the dog" },
+          { "name": "feed the lawn" },
+          { "name": "pay bills" },
+          { "name": "run" },
+          { "name": "swim" },
+     ]
 });   
