8b27e90c53e89cd3b520809bf7ec55d600ea8432
brouchey <benrouch@me.com>
2016-12-10 14:33:48 +0100

    create first controller mainCtrl

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,6 @@
-angular.module("todoListApp", []);     // creates new module, second parameter defines dependencies
+angular.module("todoListApp", [])		// creates new module, second parameter defines dependencies
+.controller('mainCtrl', function($scope) {     // $scope = area of operation
+     $scope.helloWorld = function() {
+          console.log("Hello there! This is the helloWorld controller function.");
+     };
+});   
