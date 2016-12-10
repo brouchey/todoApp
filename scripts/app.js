86137d2fb5e1836d3f8ec3af9c9dfd61d1c96179
brouchey <benrouch@me.com>
2016-12-10 15:12:54 +0100

    add a first service

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,9 @@
 angular.module("todoListApp", [])		// creates new module, second parameter defines dependencies
-.controller('mainCtrl', function($scope) {		// $scope = area of operation
+.controller('mainCtrl', function($scope, dataService) {
+		// $scope = this area of operation
+		// dataService = dependency in the controller, now we can access the services methods as any other object
+
+		$scope.helloConsole = dataService.helloConsole;
 
      $scope.learningNgChange = function() {		// just to see how it works
           console.log("An input changed !");
@@ -13,4 +17,9 @@ angular.module("todoListApp", [])		// creates new module, second parameter defin
           { "name": "run" },
           { "name": "swim" },
      ]
+})
+.service('dataService', function() {
+     this.helloConsole = function() {
+          console.log('This is the Hello Console service !');     // testing
+     }
 });   
